a722914d9c636d6579b38ede9608aa889683907f
Boudin <boudin321@gmail.com>
2025-02-28 09:13:41 -0500

    changes
    changes

--- WindowsTroubleShooter/NetworkCheck.cs
+++ WindowsTroubleShooter/NetworkCheck.cs
@@ -9,21 +9,25 @@ namespace WindowsTroubleShooter
 {
     internal class NetworkCheck
     {
+        // Method to check if the internet is available
         internal static bool IsInternetAvailable()
         {
             try
             {
+                // Create a new Ping instance
                 using (Ping ping = new Ping())
                 {
+                    // Send a ping to www.google.com with a timeout of 2000 ms
                     PingReply reply = ping.Send("www.google.com", 2000); // 3000 ms timeout
+                    // Return true if the ping was successful
                     return reply.Status == IPStatus.Success;
                 }
             }
             catch
             {
+                // Return false if an exception occurs (e.g., no internet connection)
                 return false;
             }
         }
-        
     }
 }
