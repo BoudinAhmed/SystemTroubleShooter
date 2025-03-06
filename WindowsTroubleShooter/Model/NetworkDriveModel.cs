9e299cb4d2384d1fb22e47cbba00f15ffb545bf1
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2025-03-06 14:20:53 -0500

    Added a Network Drive model

---
+++ WindowsTroubleShooter/Model/NetworkDriveModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsTroubleShooter.Model
+{
+    internal class NetworkDriveModel
+    {
+        public string MapNetworkDrive(char letter, string path)
+        {
+            string cmd = "net";
+            string command = @$"use {letter}: {path}";
+            
+            try
+            {
+                //open cmd and map network drive
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = cmd;
+                p.StartInfo.Arguments = $" {command}";
+                p.StartInfo.CreateNoWindow = true;
+                p.Start();
+                p.WaitForExit();
+                string output = p.StandardOutput.ReadToEnd();
+                p.Dispose();
+
+                return $"Netword drive {letter} mapped ";
+            }
+
+            catch 
+            {
+                return "Please verify your internet connection and/or VPN if working from home";
+            }
+        }
+    }
+}
--- WindowsTroubleShooter/obj/Debug/net6.0-windows/WindowsTroubleShooter.AssemblyInfo.cs
+++ WindowsTroubleShooter/obj/Debug/net6.0-windows/WindowsTroubleShooter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowsTroubleShooter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d1432b79006fb15bcfc7ff322d8df74b75f08f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowsTroubleShooter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowsTroubleShooter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
