d15fb7317c5bf0ee2128d888643438dcd7a6d26d
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2025-03-08 19:00:19 -0500

    converting project to MVVM

---
+++ WindowsTroubleShooter/ViewModel/IssueSelectedViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Caliburn.Micro;
+using System.Collections.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using GalaSoft.MvvmLight;
+
+namespace WindowsTroubleShooter.ViewModel
+{
+    class IssueSelectedViewModel : ObservableObject
+    {
+        public ObservableCollection<string> SelectIssues { get; } = new();
+        public ICommand StartTroubleshootingCommand { get; }
+
+
+    }
+}
