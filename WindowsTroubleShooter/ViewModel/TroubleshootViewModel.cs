1eecd4df80ba2793e72d00bf1f3ba6431c67ffc1
Boudin <boudin321@gmail.com>
2025-03-11 16:28:36 -0400

    bugs fixed to Viewmodel to View binding

--- WindowsTroubleShooter/ViewModel/TroubleshootViewModel.cs
+++ WindowsTroubleShooter/ViewModel/TroubleshootViewModel.cs
@@ -8,17 +8,21 @@ using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace WindowsTroubleShooter.ViewModel
 {
-    public class TroubleshootViewModel : INotifyPropertyChanged
+    public class TroubleshootViewModel : INotifyPropertyChanged, IIssueViewModel
     {
-     
+        public TroubleshootViewModel()
+        {
+        }
+
+        public String StatusMessage { get; set; }
         public List<IIssueViewModel> IssuesToTroubleshoot = new List<IIssueViewModel>();
         
         public ICommand RunDiagnosticsCommand { get; }
-
 
         public TroubleshootViewModel(ObservableCollection<string> selectedIssues)
         {
@@ -28,7 +32,11 @@ namespace WindowsTroubleShooter.ViewModel
                 switch (issue)
                 {
                     case "NetworkDrive":
+                        var networkDriveViewModel = new NetworkDriveViewModel();
+                        networkDriveViewModel.PropertyChanged += IssueStatusChanged;
                         IssuesToTroubleshoot.Add(new NetworkDriveViewModel());
+
+                        
                         break;
                     case "SearchBar":
                         //_issuesToTroubleshoot.Add(new SearchBarViewModel());
@@ -37,7 +45,7 @@ namespace WindowsTroubleShooter.ViewModel
                 }
             }
 
-            RunDiagnosticsCommand = new RelayCommand(async () => await RunDiagnosticsAsync());
+            Task.Run(async ()=> await RunDiagnosticsAsync());
         }
 
 
@@ -47,15 +55,36 @@ namespace WindowsTroubleShooter.ViewModel
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
+        private void IssueStatusChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(IIssueViewModel.StatusMessage))
+            {
+                var issue = sender as IIssueViewModel;
+                if (issue != null)
+                {
+                    StatusMessage = issue.StatusMessage; // Update the main StatusMessage property
+                }
+            }
+        }
 
         public async Task RunDiagnosticsAsync()
         {
-            foreach(var issue in IssuesToTroubleshoot)
+            // Run diagnostics in a background task to avoid blocking the UI thread.
+            await Task.Run(async () =>
             {
+                foreach (var issue in IssuesToTroubleshoot)
+                {
+                    await issue.RunDiagnosticsAsync();
 
-                await issue.RunDiagnosticsAsync();
-            }
+                    // To update the UI, use the Dispatcher to marshal the updates to the UI thread.
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        StatusMessage = issue.StatusMessage;
+                    });
+
+                    await Task.Delay(500); // Add delay if needed
+                }
+            });
         }
     }
 }
