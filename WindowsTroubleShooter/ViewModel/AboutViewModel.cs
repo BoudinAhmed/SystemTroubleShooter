f6bd7b9e375e6709c10decf031e547c9f4c9cd1d
Boudin <boudin321@gmail.com>
2025-04-29 15:47:42 -0400

    Version has been assigned in the vm as opposed to directly from the view

--- WindowsTroubleShooter/ViewModel/AboutViewModel.cs
+++ WindowsTroubleShooter/ViewModel/AboutViewModel.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace WindowsTroubleShooter.ViewModel
 {
-    public class AboutViewModel
+    public class AboutViewModel : ViewModelBase
     {
+        private string _version;
+
+        public string Version
+        {
+            get =>  _version; 
+            set => SetProperty(ref _version, value);
+        }
+
+        public AboutViewModel() 
+        { 
+        LoadVersionInfo();
+        }
+        private void LoadVersionInfo()
+        {
+            // Hardcoded for now but will load it from assembly info
+            Version = "Version 2.0.1";
+        }
     }
 }
