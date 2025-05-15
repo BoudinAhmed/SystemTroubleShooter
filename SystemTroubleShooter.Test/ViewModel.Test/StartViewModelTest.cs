cb3c58d6f1df1edf822f218a348c604dde91b6a4
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2025-05-15 19:33:17 -0400

    Refractor: solution, projects, and folders to SystemTroubleShooter

---
+++ SystemTroubleShooter.Test/ViewModel.Test/StartViewModelTest.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemTroubleShooter.View;
+using SystemTroubleShooter.ViewModel;
+
+namespace SystemTroubleShooter.Test.ViewModel.Test
+{
+
+
+    public class StartViewModelTests
+    {
+        [Fact]
+        public void SwitchToDashboardCommand_SetsCurrentUserControlToDashboardView()
+        {
+            // Arrange
+            var viewModel = new StartViewModel();
+            viewModel.SwitchToHomeCommand.Execute(null); // Navigate away
+
+            // Act
+            viewModel.SwitchToSystemOverviewCommand.Execute(null); // Switch back to Dashboard
+
+            // Assert
+            Assert.IsType<SystemOverviewViewModel>(viewModel.CurrentContentViewModel);
+            Assert.Equal("SystemOverview", viewModel.SelectedView);
+        }
+
+        [Fact]
+        public void SwitchToProblemListCommand_SetsCurrentContentViewModelToProblemListViewModel()
+        {
+            // Arrange
+            var viewModel = new StartViewModel();
+
+            // Act
+            viewModel.SwitchToHomeCommand.Execute(null); // Switch to ProblemList
+
+            // Assert
+            Assert.IsType<HomeViewModel>(viewModel.CurrentContentViewModel);
+            Assert.Equal("Home", viewModel.SelectedView);
+        }
+
+        [Fact]
+        public void SwitchToSettingsCommand_SetsCurrentContentViewModelToSettingsViewModel()
+        {
+            // Arrange
+            var viewModel = new StartViewModel();
+
+            // Act
+            viewModel.SwitchToSettingsCommand.Execute(null); // Switch to Settings
+
+            // Assert
+            Assert.IsType<SettingsViewModel>(viewModel.CurrentContentViewModel);
+            Assert.Equal("Settings", viewModel.SelectedView);
+        }
+
+        [Fact]
+        public void SwitchToAboutCommand_SetsCurrentContentViewModelToAboutViewModel()
+        {
+            // Arrange
+            var viewModel = new StartViewModel();
+
+            // Act
+            viewModel.SwitchToAboutCommand.Execute(null); // Switch to About
+
+            // Assert
+            Assert.IsType<AboutViewModel>(viewModel.CurrentContentViewModel);
+            Assert.Equal("About", viewModel.SelectedView);
+        }
+
+    }
+}
---
+++ SystemTroubleShooter/Helpers/Converters/IndexToColumnConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows;
+
+namespace SystemTroubleShooter.Helpers.Converters
+{
+    public class IndexToColumnConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ContentPresenter contentPresenter)
+            {
+                var itemsControl = ItemsControl.ItemsControlFromItemContainer(contentPresenter);
+                if (itemsControl != null)
+                {
+                    int index = itemsControl.ItemContainerGenerator.IndexFromContainer(contentPresenter);
+                    return index % 2;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IndexToRowConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ContentPresenter contentPresenter)
+            {
+                var itemsControl = ItemsControl.ItemsControlFromItemContainer(contentPresenter);
+                if (itemsControl != null)
+                {
+                    int index = itemsControl.ItemContainerGenerator.IndexFromContainer(contentPresenter);
+                    return index / 2 + 2;
+                }
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
