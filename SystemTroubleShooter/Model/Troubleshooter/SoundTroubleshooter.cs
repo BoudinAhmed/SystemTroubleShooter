477652b04207dfbed42ecafc666f9cbaeb95c80e
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2025-05-19 20:04:56 -0400

    Feature: sound troubleshooter model. Currently getting default audio volume and if mic is muted or not

---
+++ SystemTroubleShooter/Model/Troubleshooter/SoundTroubleshooter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
+
+namespace SystemTroubleShooter.Model.Troubleshooter
+{
+    public class SoundTroubleshooter : BaseTroubleshooter
+    {
+        public static float GetMasterVolumeNAudio()
+        {
+            // Releases COM objects correctly and to ensure Dispose is called
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
+                {
+                    if (device != null && device.AudioEndpointVolume != null)
+                    {
+                        float volume = device.AudioEndpointVolume.MasterVolumeLevelScalar * 100;
+                        return volume;
+                    }
+                    return -1f; 
+                }
+            }
+        }
+
+        public static bool IsMasterVolumeMutedNAudio()
+        {
+            // Releases COM objects correctly and to ensure Dispose is called
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                using (var device = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia))
+                {
+                    if (device != null && device.AudioEndpointVolume != null)
+                    {
+                        bool muted = device.AudioEndpointVolume.Mute;
+                        return muted;
+                    }
+                    // Todo: Handle device or volume control is not available
+                    
+                    
+
+                    throw new InvalidOperationException("Could not retrieve default audio device or its volume control.");
+                }
+            }
+        }
+
+
+        public override async Task<string> RunDiagnosticsAsync()
+        {
+
+            
+            
+            return "te";
+        }
+    }
+}
