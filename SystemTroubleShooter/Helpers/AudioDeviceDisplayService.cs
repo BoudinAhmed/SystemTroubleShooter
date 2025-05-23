cc40ae276ec450f6533bfc535020000dee7936a0
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2025-05-22 21:18:37 -0400

    Feature: new window (view/viewmodel) to help the user select which audio device is the problem

---
+++ SystemTroubleShooter/Helpers/AudioDeviceDisplayService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using SystemTroubleShooter.Interfaces;
+using SystemTroubleShooter.View;
+using SystemTroubleShooter.ViewModel;
+
+namespace SystemTroubleShooter.Helpers
+{
+    public class AudioDeviceDisplayService : IAudioDeviceDisplayService
+    {
+       
+
+        public void DisplayAudioDevices(List<string> outputDevices, List<string> inputDevices)
+        {
+            var audioDeviceView = new SoundDevicesView();
+            audioDeviceView.DataContext = new SoundDevicesViewModel();
+            audioDeviceView.Show();
+           
+        }
+    }
+}
---
+++ SystemTroubleShooter/View/SoundDevicesView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace SystemTroubleShooter.View
+{
+    /// <summary>
+    /// Interaction logic for SoundDevicesView.xaml
+    /// </summary>
+    public partial class SoundDevicesView : Window
+    {
+        public SoundDevicesView()
+        {
+            InitializeComponent();
+        }
+    }
+}
